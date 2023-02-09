44c50ca3e54d311235f73dfd7a762b92eacf453d
dmitry-sukhoruchkin <dmitry.sukhoruchkin@gmail.com>
2023-02-10 00:36:53 +0300

    Add files via upload

---
+++ Arduino/BLE_uart_freeeeg16/BLE_uart_freeeeg16.ino
@@ -0,0 +1,157 @@
+/*
+    Video: https://www.youtube.com/watch?v=oCMOYS71NIU
+    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
+    Ported to Arduino ESP32 by Evandro Copercini
+
+   Create a BLE server that, once we receive a connection, will send periodic notifications.
+   The service advertises itself as: 6E400001-B5A3-F393-E0A9-E50E24DCCA9E
+   Has a characteristic of: 6E400002-B5A3-F393-E0A9-E50E24DCCA9E - used for receiving data with "WRITE" 
+   Has a characteristic of: 6E400003-B5A3-F393-E0A9-E50E24DCCA9E - used to send data with  "NOTIFY"
+
+   The design of creating the BLE server is:
+   1. Create a BLE Server
+   2. Create a BLE Service
+   3. Create a BLE Characteristic on the Service
+   4. Create a BLE Descriptor on the characteristic
+   5. Start the service.
+   6. Start advertising.
+
+   In this example rxValue is the data received (only accessible inside that function).
+   And txValue is the data to be sent, in this example just a byte incremented every second. 
+*/
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+#include <esp_attr.h>
+
+BLEServer *pServer = NULL;
+BLECharacteristic * pTxCharacteristic;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+uint8_t txValue = 0;
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
+#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
+#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+
+uint8_t unitMACAddress[6];  // Use MAC address in BT broadcast and display
+char deviceName[20];        // The serial string that is broadcast.
+
+#define DATA_SIZE 57    // 26 bytes is a lower than RX FIFO size (127 bytes) 
+#define BAUD 115200       // Any baudrate from 300 to 115200
+#define TEST_UART 1     // Serial1 will be used for the loopback testing with different RX FIFO FULL values
+#define RXPIN 2         // GPIO 4 => RX for Serial1
+#define TXPIN 3         // GPIO 5 => TX for Serial1
+const int BUFFER_SIZE = DATA_SIZE;
+uint8_t serialReadBuffer[BUFFER_SIZE];
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string rxValue = pCharacteristic->getValue();
+
+      if (rxValue.length() > 0) {
+//        auto count = rxValue.length();
+//        Serial1.write(rxValue, count);
+//        Serial.println("*********");
+//        Serial.print("Received Value: ");
+//        for (int i = 0; i < rxValue.length(); i++)
+//          Serial.print(rxValue[i]);
+
+//        Serial.println();
+//        Serial.println("*********");
+      }
+    }
+};
+
+
+void setup() {
+  // UART1 will have its RX<->TX cross connected
+  // GPIO4 <--> GPIO5 using external wire
+  Serial1.begin(BAUD, SERIAL_8N1, RXPIN, TXPIN); // Rx = 4, Tx = 5 will work for ESP32, S2, S3 and C3
+
+  Serial.begin(115200);
+
+  esp_read_mac(unitMACAddress, ESP_MAC_WIFI_STA);
+  
+  // Convert MAC address to Bluetooth MAC (add 2): https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/system/system.html#mac-address
+  unitMACAddress[5] += 2;                                                          
+  
+  //Create device name
+  sprintf(deviceName, "BleBridge-%02X%02X", unitMACAddress[4], unitMACAddress[5]); 
+
+  // Create the BLE Device
+  BLEDevice::init(deviceName);
+
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  pTxCharacteristic = pService->createCharacteristic(
+										CHARACTERISTIC_UUID_TX,
+										BLECharacteristic::PROPERTY_NOTIFY
+									);
+                      
+  pTxCharacteristic->addDescriptor(new BLE2902());
+
+  BLECharacteristic * pRxCharacteristic = pService->createCharacteristic(
+											 CHARACTERISTIC_UUID_RX,
+											BLECharacteristic::PROPERTY_WRITE
+										);
+
+  pRxCharacteristic->setCallbacks(new MyCallbacks());
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  pServer->getAdvertising()->start();
+  Serial.println("Waiting a client connection to notify...");
+}
+
+void loop() {
+
+    if (deviceConnected) {
+        if (Serial1.available()) {
+            auto count = Serial1.readBytes(serialReadBuffer, DATA_SIZE);
+            pTxCharacteristic->setValue(serialReadBuffer, count);
+//            txValue = Serial.read();
+//            pTxCharacteristic->setValue(&txValue, 1);
+            pTxCharacteristic->notify();
+        }
+//        pTxCharacteristic->setValue(&txValue, 1);
+//        pTxCharacteristic->notify();
+//        txValue++;
+//		delay(10); // bluetooth stack will go into congestion, if too many packets are sent
+	}
+
+    // disconnecting
+    if (!deviceConnected && oldDeviceConnected) {
+        delay(500); // give the bluetooth stack the chance to get things ready
+        pServer->startAdvertising(); // restart advertising
+        Serial.println("start advertising");
+        oldDeviceConnected = deviceConnected;
+    }
+    // connecting
+    if (deviceConnected && !oldDeviceConnected) {
+		// do stuff here on connecting
+        oldDeviceConnected = deviceConnected;
+    }
+}
