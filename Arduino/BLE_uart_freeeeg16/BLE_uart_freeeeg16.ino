e1f30cc0dd3f4bda71619cbdbf58c0a70c0f2e3a
dmitry-sukhoruchkin <dmitry.sukhoruchkin@gmail.com>
2023-04-03 02:24:02 +0300

    Add files via upload

--- Arduino/BLE_uart_freeeeg16/BLE_uart_freeeeg16.ino
+++ Arduino/BLE_uart_freeeeg16/BLE_uart_freeeeg16.ino
@@ -25,6 +25,11 @@
 #include <BLE2902.h>
 #include <esp_attr.h>
 
+// #include <WiFi.h>
+// WiFi.disconnect(true);
+// WiFi.mode(WIFI_OFF);
+// btStop();
+
 BLEServer *pServer = NULL;
 BLECharacteristic * pTxCharacteristic;
 bool deviceConnected = false;
@@ -50,6 +55,7 @@ char deviceName[20];        // The serial string that is broadcast.
 #define TXPIN 6         // GPIO 5 => TX for Serial1
 const int BUFFER_SIZE = DATA_SIZE;
 uint8_t serialReadBuffer[BUFFER_SIZE];
+char charrxValue[BUFFER_SIZE];
 
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
@@ -66,8 +72,9 @@ class MyCallbacks: public BLECharacteristicCallbacks {
       std::string rxValue = pCharacteristic->getValue();
 
       if (rxValue.length() > 0) {
-//        auto count = rxValue.length();
-//        Serial1.write(rxValue, count);
+        auto count = rxValue.length();
+        strcpy(charrxValue, rxValue.c_str());  // copy from str to char[]
+        Serial1.write(charrxValue, count);
 //        Serial.println("*********");
 //        Serial.print("Received Value: ");
 //        for (int i = 0; i < rxValue.length(); i++)
@@ -98,6 +105,17 @@ void setup() {
 
   // Create the BLE Device
   BLEDevice::init(deviceName);
+
+  // esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_DEFAULT, ESP_PWR_LVL_P9);
+  // esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_CONN_HDL0, ESP_PWR_LVL_P9);
+  // esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_CONN_HDL1, ESP_PWR_LVL_P9);
+  // esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_CONN_HDL2, ESP_PWR_LVL_P9);
+  // esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_CONN_HDL3, ESP_PWR_LVL_P9);
+  // esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_CONN_HDL4, ESP_PWR_LVL_P9);
+  // esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_CONN_HDL5, ESP_PWR_LVL_P9);
+  // esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_CONN_HDL6, ESP_PWR_LVL_P9);
+  // esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_CONN_HDL8, ESP_PWR_LVL_P9);
+  // esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_ADV, ESP_PWR_LVL_P9);
 
   // Create the BLE Server
   pServer = BLEDevice::createServer();
